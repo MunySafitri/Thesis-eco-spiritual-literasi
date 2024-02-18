22e36ed8db699ec76b21e482ef8788038bea241d
Naufal Angkasah <ahmad.naufalangkasah@gmail.com>
2024-02-18 11:09:03 +0700

    pembaruan futer ver 3

--- src/components/Layouts/Footer.vue
+++ src/components/Layouts/Footer.vue
@@ -5,32 +5,33 @@
       <div class="row justify-content-center">
         <div class="col-sm-6 col-lg-3">
           <div class="single-footer-widget">
-            <div class="footer-logo" >
+            <div class="footer-logo">
               <a :href="'#'">
-                <img src="../../assets/img/logoupimerah.png" alt="Logo"  />
+                <img src="../../assets/img/logoupimerah.png" alt="Logo" />
               </a>
               <p>
-                Supporter.C is a Platform as a Service (PaaS) serving as the Digital Infrastructure at the Digital Service Division of PT Telekomunikasi Indonesia. 
+                Supporter.C is a website creation service platform according to
+                customer requests
               </p>
               <ul class="footer-social">
                 <li>
                   <a :href="''">
-                    <i class="bi-facebook" style=" padding-left: 1px"></i>
+                    <i class="bi-facebook" style="padding-left: 1px"></i>
                   </a>
                 </li>
                 <li>
                   <a :href="''">
-                    <i class="bi-twitter" style=" padding-left: 1px"></i>
+                    <i class="bi-twitter" style="padding-left: 1px"></i>
                   </a>
                 </li>
                 <li>
                   <a :href="''">
-                    <i class="bi-linkedin" style=" padding-left: 1px"></i>
+                    <i class="bi-linkedin" style="padding-left: 1px"></i>
                   </a>
                 </li>
                 <li>
                   <a :href="''">
-                    <i class="bi-instagram" style=" padding-left: 1px"></i>
+                    <i class="bi-instagram" style="padding-left: 1px"></i>
                   </a>
                 </li>
               </ul>
@@ -42,16 +43,24 @@
             <h3>Supporter.C</h3>
             <ul class="quick-link">
               <li>
-                <a  @click="scroll('Overview')" :href="'javascript:void(0)'">Overview</a>
+                <a @click="scroll('Overview')" :href="'javascript:void(0)'"
+                  >Overview</a
+                >
               </li>
               <li>
-                <a  @click="scroll('Features')" :href="'javascript:void(1)'">Features</a>
+                <a @click="scroll('Features')" :href="'javascript:void(1)'"
+                  >Features</a
+                >
               </li>
               <li>
-                <a  @click="scroll('Steps')" :href="'javascript:void(2)'">How To</a>
+                <a @click="scroll('Steps')" :href="'javascript:void(2)'"
+                  >How To</a
+                >
               </li>
               <li>
-                <a  @click="scroll('Order')" :href="'javascript:void(3)'">Order</a>
+                <a @click="scroll('Order')" :href="'javascript:void(3)'"
+                  >Order</a
+                >
               </li>
             </ul>
           </div>
@@ -61,10 +70,14 @@
             <h3>Company</h3>
             <ul class="quick-link">
               <li>
-                <a :href="''" target="_blank">Universitas Pendidikan Indonesia</a>
+                <a :href="'https://www.upi.edu/'" target="_blank"
+                  >Universitas Pendidikan Indonesia</a
+                >
               </li>
               <li>
-                <a :href="''" target="_blank">About Universitas Pendidikan Indonesia</a>
+                <a :href="'https://www.upi.edu/about'" target="_blank"
+                  >About Universitas Pendidikan Indonesia</a
+                >
               </li>
             </ul>
           </div>
@@ -74,14 +87,23 @@
             <h3>About Us</h3>
             <ul class="quick-link">
               <li>
-                <a @click="scroll('Overview')" :href="'javascript:void(0)'">Supporter.C</a>
+                <a @click="scroll('Overview')" :href="'javascript:void(0)'"
+                  >Supporter.C</a
+                >
               </li>
               <li>
-                <a :href="''" target="_blank">IndigoHub Bandung</a>
+                <a
+                  :href="'https://informatika.usk.ac.id/webinf/'"
+                  target="_blank"
+                  >Informatika USK</a
+                >
               </li>
               <li>
-                <a :href="''"
-                  >Jl. Gegerkalong Hilir No.47 Kota Bandung, Jawa Barat 40152</a
+                <a
+                  :href="'https://maps.app.goo.gl/hjZuxKQxEbDdYG9e6'"
+                  target="_blank"
+                  >JL. Syech Abdurrauf No.3, Kopelma Darussalam, Kec. Syiah
+                  Kuala, Kota Banda Aceh, Aceh 23111</a
                 >
               </li>
             </ul>
@@ -93,17 +115,19 @@
             <ul class="address-info">
               <li>
                 <i class="bi-telephone-outbound"></i>
-                <a :href="'tel:+6282228000720'">+62 822 2800 0720</a>
-              </li>
-              <li>
-                <i class="bi-book"></i>
-                <a :href="''"
-                  >Helpdeskdxb@telkom.co.id</a
+                <a :href="'tel:+6282367555945'" target="_blank"
+                  >+62 823 6755 5945</a
                 >
               </li>
               <li>
-                <i class="bi bi-telegram"></i>
-                <a :href="''">helpdeskdb_bot</a>
+                <i class="bi-envelope"></i>
+                <a :href="''">ahmad.naufalangkasah@gmail.com</a>
+              </li>
+              <li>
+                <i class="bi bi-whatsapp"></i>
+                <a :href="'https://wa.me/qr/YLKG6DUJ3WREC1'" target="_blank"
+                  >+62 823 6755 5945</a
+                >
               </li>
             </ul>
           </div>
@@ -114,7 +138,8 @@
           <div class="col-lg-6 col-md-6">
             <div class="copyright-item">
               <p>
-                &copy; Copyright {{ currentYear }} By <a href="#">Supporter.C</a>
+                &copy; Copyright {{ currentYear }} By
+                <a href="#">Supporter.C</a>
               </p>
             </div>
           </div>
@@ -126,29 +151,28 @@
 <!-- eslint-disable prettier/prettier -->
 <script>
 export default {
-  name: "AppFooter",
+  name: 'AppFooter',
   data() {
     return {
       currentYear: new Date().getFullYear(),
-    };
-  },
-  mounted() {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY >= 100) {
-        this.isSticky = true;
-      } else {
-        this.isSticky = false;
-      }
-    });
-  },
-  methods: {
-  scroll(refName) {
-    const element = document.getElementById(refName);
-    if (element) {
-      element.scrollIntoView({ top: element.offsetTop, behavior: "smooth" });
     }
   },
-},
-
-};
+  mounted() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 100) {
+        this.isSticky = true
+      } else {
+        this.isSticky = false
+      }
+    })
+  },
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName)
+      if (element) {
+        element.scrollIntoView({ top: element.offsetTop, behavior: 'smooth' })
+      }
+    },
+  },
+}
 </script>
